41540fe93ac1c65070ebc0fc033b9cafc9b6b682
mamun-prodhan <mamunpr9@gmail.com>
2023-02-02 11:53:38 +0600

    onclick added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import { LoadingButton } from '@mui/lab';
 import { Button } from '@mui/material';
 import { Stack } from '@mui/system';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(()=>{
+    const id = setTimeout(() =>{
+      setLoading(false);
+    }, 1000);
+
+    return ()=>{
+      clearTimeout(id);
+    };
+  }, [loading])
+
   return (
     <div className="app">
       <div
@@ -18,7 +32,14 @@ function App() {
         <Stack spacing={2} direction="row">
         <Button variant='contained' color='error'>Checkout</Button>
         <Button variant='outlined'>Checkout</Button>
-        <Button variant='text'>Checkout</Button>
+        <LoadingButton
+          loading={loading}
+          loadingIndicator='Loading...'
+          variant='outline'
+          onClick={()=> setLoading(true)}
+        >
+          Fetch Data
+        </LoadingButton>
         </Stack>
 
 
