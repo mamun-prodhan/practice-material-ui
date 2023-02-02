73c9a3cc7be2c2025ffbd17d20f54c329952a485
mamun-prodhan <mamunpr9@gmail.com>
2023-02-02 11:15:54 +0600

    items moved in middle point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div
+      style={{
+        background: '#cfcccc',
+        width: '400px',
+        height: '400px',
+        borderRadius: '1rem',
+        padding: '2rem',
+      }}
+      >
+      </div>
     </div>
   );
 }
